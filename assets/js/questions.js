0b111fff4383b7946659c564b8d4256b2786462f
Thoma77s <thomassiffc3@gmail.com>
2021-12-22 13:35:46 -0500

    added questions number

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -67,4 +67,48 @@ let questions = [
       "option 4"
     ]
   },
+  {
+    numb: 7,
+    question: "Your Question is Here",
+    answer: "Correct answer of the question is here",
+    options: [
+      "Option 1",
+      "option 2",
+      "option 3",
+      "option 4"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Your Question is Here",
+    answer: "Correct answer of the question is here",
+    options: [
+      "Option 1",
+      "option 2",
+      "option 3",
+      "option 4"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Your Question is Here",
+    answer: "Correct answer of the question is here",
+    options: [
+      "Option 1",
+      "option 2",
+      "option 3",
+      "option 4"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Your Question is Here",
+    answer: "Correct answer of the question is here",
+    options: [
+      "Option 1",
+      "option 2",
+      "option 3",
+      "option 4"
+    ]
+  },
 ];
